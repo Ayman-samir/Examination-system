423652df4a22a79509e6600c0e6fe134277759c8
Ayman samir <samirbelal589@gmail.com>
2023-02-18 01:40:27 +0200

    Department_table

---
+++ Tables/dbo.Department.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Department] (
+  [Id] [int] NULL,
+  [Name] [varchar](50) NOT NULL,
+  [Description] [varchar](50) NULL,
+  [Location] [varchar](50) NULL,
+  [MangerId] [varchar](50) NULL,
+  CONSTRAINT [PK_Department] PRIMARY KEY CLUSTERED ([Name])
+)
+ON [PRIMARY]
+GO
