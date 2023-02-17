4a853aeb4b1a824b01fd0e55ce6dc53e6149b8c8
Ayman samir <samirbelal589@gmail.com>
2023-02-18 01:47:55 +0200

    student_table

---
+++ Tables/dbo.Student.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Student] (
+  [Id] [int] NULL,
+  [FName] [varchar](50) NULL,
+  [LName] [varchar](50) NULL,
+  [City] [varchar](50) NULL,
+  [Street] [varchar](50) NULL,
+  [Building] [varchar](50) NULL,
+  [DOB] [varchar](50) NULL,
+  [Email] [varchar](50) NULL,
+  [Dept_Id] [varchar](50) NULL
+)
+ON [PRIMARY]
+GO
