824ff8325169c9d5c875ff3d50e9ea6917109159
samah8 <samahayman836@gmail.com>
2023-02-19 20:29:47 +0200

    instructor

---
+++ Tables/StaticData/dbo.Instructor.Data.sql
@@ -0,0 +1,2 @@
+﻿INSERT dbo.Instructor(Id, Name, Salary, Email, Dept_Id) VALUES (1, 'amr', 50000, 'amr123@gmail.com', 1);
+INSERT dbo.Instructor(Id, Name, Salary, Email, Dept_Id) VALUES (2, 'nada', 60000, 'nada@gmail.com', 2);
