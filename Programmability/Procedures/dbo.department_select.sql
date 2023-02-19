0b70bd304509bf23e49e8ff561ecddc9fdcafddf
Ayman-samir <84547082+Ayman-samir@users.noreply.github.com>
2023-02-19 18:42:28 +0200

    Delete Programmability/Procedures directory

--- Programmability/Procedures/dbo.department_select.sql
+++
@@ -1,8 +0,0 @@
-﻿SET QUOTED_IDENTIFIER, ANSI_NULLS ON
-GO
-CREATE PROC [dbo].[department_select] 
-AS
-SELECT * FROM Department 
-
-department_select 
-GO
--- Programmability/Procedures/dbo.insert_department.sql
+++
@@ -1,8 +0,0 @@
-﻿SET QUOTED_IDENTIFIER, ANSI_NULLS ON
-GO
-CREATE PROC [dbo].[insert_department] @name VARCHAR(50),@description VARCHAR(50),@location VARCHAR(50),@manger_id INT
-AS 
-
-INSERT INTO Department 
-VALUES(@name,@description,@location,@manger_id)
-GO
