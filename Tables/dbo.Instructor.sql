ab4989c6db77ac01ba26e4a9050b7e93372feec0
samah8 <samahayman836@gmail.com>
2023-02-19 20:26:56 +0200

    instructor

--- Tables/dbo.Instructor.sql
+++ Tables/dbo.Instructor.sql
@@ -1,9 +1,10 @@
 ﻿CREATE TABLE [dbo].[Instructor] (
-  [Id] [int] NULL,
+  [Id] [int] NOT NULL,
   [Name] [varchar](50) NULL,
   [Salary] [int] NULL,
   [Email] [varchar](50) NULL,
-  [Dept_Id] [int] NULL
+  [Dept_Id] [int] NULL,
+  CONSTRAINT [PK_Instructor] PRIMARY KEY CLUSTERED ([Id])
 )
 ON [PRIMARY]
 GO
