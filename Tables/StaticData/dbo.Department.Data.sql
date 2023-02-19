362d5381dc7ea378bc69dd7b89af706e732a89c6
Ayman samir <samirbelal589@gmail.com>
2023-02-19 19:11:23 +0200

    add data

---
+++ Tables/StaticData/dbo.Department.Data.sql
@@ -0,0 +1,5 @@
+﻿INSERT dbo.Department(Id, Name, Description, Location, MangerId) VALUES (1, 'os', 'open source', 'Mansoura', '1');
+INSERT dbo.Department(Id, Name, Description, Location, MangerId) VALUES (2, 'pd', 'professional', 'Mansoura', '2');
+INSERT dbo.Department(Id, Name, Description, Location, MangerId) VALUES (3, 'Ai', 'Artificial intiligance', 'Mansoura', '5');
+INSERT dbo.Department(Id, Name, Description, Location, MangerId) VALUES (4, 'ux', 'user exp', 'Mansoura', '4');
+INSERT dbo.Department(Id, Name, Description, Location, MangerId) VALUES (5, 'mk', 'markiting', 'Mansoura', '4');
