0161342be51416ebad6c097eb3d4aaca2c7a0be9
Ayman samir <samirbelal589@gmail.com>
2023-02-19 19:42:46 +0200

    add new reco

--- Tables/StaticData/dbo.Department.Data.sql
+++ Tables/StaticData/dbo.Department.Data.sql
@@ -3,3 +3,5 @@ INSERT dbo.Department(Id, Name, Description, Location, MangerId) VALUES (2, 'pd'
 INSERT dbo.Department(Id, Name, Description, Location, MangerId) VALUES (3, 'Ai', 'Artificial intiligance', 'Mansoura', '5');
 INSERT dbo.Department(Id, Name, Description, Location, MangerId) VALUES (4, 'ux', 'user exp', 'Mansoura', '4');
 INSERT dbo.Department(Id, Name, Description, Location, MangerId) VALUES (5, 'mk', 'markiting', 'Mansoura', '4');
+
+INSERT dbo.Department(Id, Name, Description, Location, MangerId) VALUES (6, 'pd', 'prof', 'mansoura', '5');
